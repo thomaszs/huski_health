a03019e0f4f0d0ecce813eb744b7a1043e7bdda4
peachykeen5 <lexi.keen@gmail.com>
2018-07-27 22:44:39 +0000

    adding new components and updating state and props

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
 
+// import NavBar from 'NavBar.jsx';
+// import PetProfile from 'PetProfile.jsx';
+// import PetChart from 'PetChart.jsx';
+// import PetActivity from 'PetActivity.jsx';
+// import StatusBar from 'StatusBar.jsx';
+
 class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id = "",
-            userName = this.props.currentUser.name,
-            currentPet = this.props.pet
+            // id: "",
+            // userName: this.props.currentUser.name,
+            // currentPet: this.props.pet
         }
     }
 
     render() {
         return(
             <div>
-                < NavBar />
+                {/* < NavBar /> */}
             <div className="container-fluid">
 
               <div className="row">
                 <div className="col-sm-3">
-                    < PetProfile pet={this.state.currentPet} />
+                    {/* < PetProfile pet={this.state.currentPet} /> */}
                 </div>
               </div>
                 <div className="col-sm-9">
                   <div className="row">
                     <div className="col-sm-8">
-                    < PetChart pet={this.state.currentPet} />
-                    < PetActivity pet={this.state.currentPet} />
+                    {/* < PetChart pet={this.state.currentPet} />
+                    < PetActivity pet={this.state.currentPet} /> */}
 
                     </div>
 
                     <div className="col-sm-4">
-                          < CurrentWeight weight={this.state.currentPet}/> 
+                          {/* < CurrentWeight weight={this.state.currentPet}/>  */}
                     </div>
                     <div className="col-sm-4">
-                          < LastFed lastFed={this.state.currentPet}/>
+                          {/* < LastFed lastFed={this.state.currentPet}/> */}
                     </div>
                     <div className="col-sm-4">
-                          < LastActive lastActive={this.state.currentPet}/>
+                          {/* < LastActive lastActive={this.state.currentPet}/> */}
                     </div>
                     <div className="col-sm-4">
-                          < DidYouKnow />
+                          {/* < DidYouKnow /> */}
                     </div>
                   </div>
                 </div>
--- src/components/pets.jsx
+++ src/components/pets.jsx
@@ -1,42 +1,21 @@
 import React, { Component } from 'react';
+import PetCard from './PetCard';
 
 class Pets extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            currentUser: {
-                name: ""
-            },
-            pets: [
-                {
-                    id: "",
-                    name: "",
-                    birthday: date,
-                    age: int,
-                    weight: "",
-                    breed: "",
-                    owner: currentUser,
-                    notes: ""
-                }
-            ]
-        }
     }
-    render() {
+    render(){
         return (
-            <div>
-                <div class="row" style="margin-left: 20px">
-                    <div class="col-sm-4">
-                        < petCard pet={this.state.pets}/>
-                    </div>
-                    <div class="col-sm-4">
-                        < petCard pet={this.state.pets}/>
-                    </div>
-                    <div class="col-sm-4 col-md-3" style="margin-top: 15em">
-                        <button type="button" class="btn btn-info btn-circle btn-xl">
-                            <i class="glyphicon glyphicon-plus"></i>
-                        </button>
-                    </div>
-
+            <div className="row" style={{ marginLeft: "20px" }}>
+                {/* for n of pets, render PetCard */}
+                <PetCard pet={this.props.pet} />
+                <PetCard pet={this.props.pet} />
+                <PetCard pet={this.props.pet} />
+                <div className="col-sm-4 col-md-3" style={{ marginTop: "15em" }}>
+                    <button type="button" className="btn btn-info btn-circle btn-xl">
+                        <i className="glyphicon glyphicon-plus"></i>
+                    </button>
                 </div>
             </div>
         );
