5a6bac8f31bbdfa5c8ebfcc73c6dc7590a148ee7
peachykeen5 <lexi.keen@gmail.com>
2018-07-27 22:48:04 +0000

    updating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import Dashboard from './components/Dashboard';
+import Pets from './components/Pets'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {
+        name: "Lexi"
+      },
+      pets: [
+        {
+          id: "1",
+          name: "Leonard",
+          birthday: "April 8, 2012",
+          age: 6,
+          weight: "22lbs",
+          breed: "Egyptian Mau",
+          owner: "Lexi",
+          notes: "These are some notes about my fat cat named Leonard."
+        }
+      ]
+    }
+  }
+
+  // set routing; based on this route render this
+  // if user is logged in, render pets
+  // if not, render log in route
+
   render() {
     return (
-      <div className="App">
-        <body id="dashboard-body" className="keen-dashboard">
-          <div id="root">
-            <Dashboard/>
-        </div>
-      </body>
-    </div>
+      < Pets pet={this.state.pets} user={this.state.currentUser}/>
     );
   }
 }
