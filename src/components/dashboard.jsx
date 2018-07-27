7b23029f5060db85c819373bdc8a36db7a93dc57
peachykeen5 <lexi.keen@gmail.com>
2018-07-27 18:36:43 +0000

    added new components folder and moved dashboard.jsx

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,7 +4,7 @@ class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentPet = this.props.pet.id
+            currentPet = this.props.pet
         }
     }
 
