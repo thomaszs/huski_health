ef55ba64cc7bda813dd007803b5679802a1721c0
kaylavu <kaylaavu@gmail.com>
2018-07-28 19:56:47 +0000

    Save Pet Profile Button sends ajax request to server

--- api_server/server.js
+++ api_server/server.js
@@ -9,18 +9,49 @@ const app = express();
 const knexConfig = require("../knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 const database = require("../database")(knex);
+const bodyParser = require("body-parser");
 
-app.use('/api')
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-app.get('/login', (req, res) => {
-   if (database.getAccount(req.body.email, req.body.password)) {
-       console.log("LOGGED IN")
-   } else {
-       console.log("FAIL")
-   }
+app.use(bodyParser.urlencoded({
+    extended:true
+})); 
+
+app.post('/api/login', (req, res) => {
+    console.log(req.body)
+    //get the DAta from the POST
+    //2. To save the data in the Database through Knex
+    //3. use .then to send the response back 
+
+   res.send({result: "It works"});
+//    if (database.getAccount(req.body.email, req.body.password)) {
+//        console.log("LOGGED IN")
+//    } else {
+//        console.log("FAIL")
+//    }
 })
 
+app.get('/api/login', (req, res) => {
+   console.log("HIT")
+//    if (database.getAccount(req.body.email, req.body.password)) {
+//        console.log("LOGGED IN")
+//    } else {
+//        console.log("FAIL")
+//    }
+})
+
+// app.post('/pet', (req, res) => {
+//     console.log("GOTTEN PET")
+//     database.getPet() 
+//  })
+
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+ console.log("Example app listening on port " + PORT);
 });
+
+
 
--- src/components/PetProfile.jsx
+++ src/components/PetProfile.jsx
@@ -1,60 +1,122 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class PetProfile extends Component {
-    render() {
-        return (
-            <div className="chart-title">
-            <div className="chart-wrapper">
-            <div className="chart-title">
-            {console.log(this.props.pet[0])}
-                      <h2>{this.props.pet[0].name}</h2>
-                      <img className="pet-img" style={{width: "100%"}} src="https://toll-imageinaboxllc.netdna-ssl.com/wp-content/uploads/2014/06/Fat-Cat_400-2.jpg" />
-                    </div>
-                    <div className="chart-stage" id="chart-01">
+  constructor(props) {
+    super(props);
+    this.state = {
+      pet: this.props.pet,
+      petName: this.props.pet[0].name, 
+      isEditing: false, 
+      test: 'hello,world',
+     };
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.editPetProfile = this.editPetProfile.bind(this);
+    this.savePetProfile = this.savePetProfile.bind(this);
+   
+  }
 
-                    </div>
-                    <div className="chart-notes">
-            <table className="table table-dark" style={{fontSize: 16}}>
-            <tbody>
-              <tr>
-                <td>Weight:</td>
-                <td>{this.props.pet[0].weight}</td>
-              </tr>
-              <tr>
-                <td>Age:</td>
-                <td>{this.props.pet[0].age}</td>
-              </tr>
-              <tr>
-                <td>Birthday:</td>
-                <td>{this.props.pet[0].birthday}</td>
-              </tr>
-              <tr>
-                <td>Breed:</td>
-                <td>{this.props.pet[0].breed}</td>
-              </tr>
-              <tr>
-                <td>Owners:</td>
-                <td>{this.props.pet[0].owner}</td>
-              </tr>
-              <tr>
-                <td>Notes:</td>
-                <td>{this.props.pet[0].notes}</td>
-              </tr>
-              <tr>
-                <td>
-                  <button type="button" className="btn btn-primary">Edit Profile</button>
-                </td>
-                <td>
-                  <button type="button" className="btn btn-success">Add Activity</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          </div>
-          </div>
-          </div>
-        ) 
+
+  toggleEdit() {
+    this.setState({isEditing: !this.state.isEditing})
+  }
+
+  editPetProfile(event) {
+    this.toggleEdit(); 
+  }
+
+  savePetProfile(event){
+    event.preventDefault(); 
+    $.ajax('http://localhost:8080/api/login', {
+      method: 'POST',
+      data: {
+        newPetName: 'newPetName'
+      }, 
+      success: function (result) {
+        console.log("Yes, it worked");
+        console.log(result); // {result: "True"}
+      },
+      error: function(err) {
+        console.log("It doesnt work")
+        }
+    });
+    console.log(event)
+  }
+
+ 
+ 
+  
+  render() {
+    if (this.state.isEditing) {
+      return (
+        <div>
+          <img className="pet-img" style={{ width: "100%" }} src="https://toll-imageinaboxllc.netdna-ssl.com/wp-content/uploads/2014/06/Fat-Cat_400-2.jpg" />
+      {/*Consider creating a EditPetProfile.jsx component. Will need to make ajax post request to the server to save new pet information*/}
+          <form>
+            Name: <input type="text" name="name" placeholder={this.state.pet[0].name}/><br/>
+            Weight:<input type="text" name="weight"/><br/>
+            Age:<input type="text" name="age"/><br/>
+            Birthday:<input type="text" name="birthday"/><br/>
+            Breed:<input type="text" name="breed"/><br/>
+            Owners:<input type="text" name="owners"/><br/>
+            Notes: <input type="text" name="notes"/><br/>
+            <button type="button" className="btn btn-primary" onClick={this.savePetProfile}>Save</button>
+          </form>
+        </div>
+      )
     }
+    return (
+      <div className="chart-title">
+        <div className="chart-wrapper">
+          <div className="chart-title">
+            <h2>{this.props.pet[0].name}</h2>
+            <img className="pet-img" style={{ width: "100%" }} src="https://toll-imageinaboxllc.netdna-ssl.com/wp-content/uploads/2014/06/Fat-Cat_400-2.jpg" />
+          </div>
+          <div className="chart-stage" id="chart-01">
+
+          </div>
+          <div className="chart-notes">
+            <table className="table table-dark" style={{ fontSize: 16 }}>
+              <tbody>
+                <tr>
+                  <td>Weight:</td>
+                  <td>{this.props.pet[0].weight}</td>
+                </tr>
+                <tr>
+                  <td>Age:</td>
+                  <td>{this.props.pet[0].age}</td>
+                </tr>
+                <tr>
+                  <td>Birthday:</td>
+                  <td>{this.props.pet[0].birthday}</td>
+                </tr>
+                <tr>
+                  <td>Breed:</td>
+                  <td>{this.props.pet[0].breed}</td>
+                </tr>
+                <tr>
+                  <td>Owners:</td>
+                  <td>{this.props.pet[0].owner}</td>
+                </tr>
+                <tr>
+                  <td>Notes:</td>
+                  <td>{this.props.pet[0].notes}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <button type="button" className="btn btn-primary" onClick={this.editPetProfile}>Edit Profile</button>
+                  </td>
+                  <td>
+                    <button type="button" className="btn btn-success">Add Activity</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default PetProfile; 
