463d447a40ba3c12974fa29d8f7cd27e7f8701c3
peachykeen5 <lexi.keen@gmail.com>
2018-07-30 20:39:42 +0000

    updating style profile

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+export default Homepage;
