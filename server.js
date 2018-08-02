eb0478c45846d8b0431ca1710120fb1bf3f05ec0
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-08-02 01:15:14 +0000

    Fixed a bug thats prevent form submission

--- server.js
+++ server.js
@@ -71,6 +71,9 @@ app.post('/api/pet/', (req, res) => {
  app.post('/api/pet/new', (req, res) => {
     // console.log(req.body)
      database.newPet(req.body)
+     .then(function (result) {
+        res.send(result)
+    })
  })
 
 
