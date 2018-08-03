0fe1e0c77bffe33d9e80cfac1330ddf0d3ace4b4
Thomas Zhang <thomas_s_zhang@yahoo.ca>
2018-08-03 21:13:22 +0000

    Added styled alert for add a new pet form

--- src/components/NewPetForm.jsx
+++ src/components/NewPetForm.jsx
@@ -10,6 +10,7 @@ import MomentLocaleUtils, {
   formatDate,
   parseDate,
 } from 'react-day-picker/moment';
+import swal from 'sweetalert'
 
 const dogBreed = require('what-dog');
 
@@ -50,7 +51,11 @@ class NewPetForm extends Component {
 
   }
   addNewPet(){ //logic to determine if it was successfully added or not
-    alert('Your new pet was successfully added!')
+    swal({
+      title: "Yay!",
+      icon: "success",
+      text:'Your new pet was successfully added!'
+    })
   } 
 
   handlePetNameChange(e) {
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -32,6 +32,5 @@ export default class SignUp extends Component {
                 </div>
             </div>
             )
-
     }
 }
