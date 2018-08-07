83ee28347a2615f75372340257759da90dc1d7ed
peachykeen5 <lexi.keen@gmail.com>
2018-08-07 21:11:24 +0000

    styling button on petcard and updating readme

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,63 +1,72 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link, Switch , Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 import FileUpload from './FileUpload';
 
 import '../css/images.css';
- 
+
 export default class Records extends Component {
-constructor(props) {
+  constructor(props) {
     super(props)
-    this.state = {images: []}
+    this.state = { images: [] }
     this.retrieveImages = this.retrieveImages.bind(this);
-}
+  }
 
-    retrieveImages() {
-        axios.get(`http://localhost:8080/api/images/${this.props.match.params.id}`, {
-        params: {
-            id: this.props.match.params.id
-        }
-    }).then((response) => {
-       console.log("Yes, it worked RECORDS");
-       console.log(response.data)
-       this.setState({images: response.data})
-       console.log(this.state.images)
-     }).catch((error) => {
-         console.log(this.props.match.params.id)
-        console.log(error);
-      })
-    }
-
-      componentDidMount() {
-        this.retrieveImages();
+  retrieveImages() {
+    axios.get(`http://localhost:8080/api/images/${this.props.match.params.id}`, {
+      params: {
+        id: this.props.match.params.id
       }
-   
-      render() {
-        if (this.state.images.length) {
-            const images = this.state.images.map((image) => {
-                return (
-                    <div className="column">
-                    <h3>{image.name}</h3>
-                        <img className="card-img-top responsive" style={{ width: "100%" }} src={`http://localhost:8080/${image.filepath}`}/>
-                        </div>
-                       )
-                    })
+    }).then((response) => {
+      console.log("Yes, it worked RECORDS");
+      console.log(response.data)
+      this.setState({ images: response.data })
+      console.log(this.state.images)
+    }).catch((error) => {
+      console.log(this.props.match.params.id)
+      console.log(error);
+    })
+  }
+
+  componentDidMount() {
+    this.retrieveImages();
+  }
+
+  render() {
+    if (this.state.images.length) {
+      const images = this.state.images.map((image) => {
         return (
-          <div class="container">
-            <div>
-              <FileUpload petid={this.props.match.params.id} retrieveImages={this.retrieveImages}/>
+          <div className="col-sm-6">
+            <div className="chart-wrapper">
+              <div className="chart-title">
+                <h3>{image.name}</h3>
+              </div>
+              <div className="chart-stage" style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                <img className="card-img-top responsive" style={{ width: "100%" }} src={`http://localhost:8080/${image.filepath}`} />
+              </div>
             </div>
             <div className="row">
-            {images}
+            <br />
             </div>
           </div>
         )
-      } else {
-          return (
-            <div class="container">
-              <FileUpload petid={this.props.match.params.id} retrieveImages={this.retrieveImages}/>
+      })
+      return (
+        <div class="container">
+          <div>
+            <FileUpload petid={this.props.match.params.id} retrieveImages={this.retrieveImages} />
           </div>
-          )
-        }
-}
+          <div className="row">
+            {images}
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div class="container">
+          <FileUpload petid={this.props.match.params.id} retrieveImages={this.retrieveImages} />
+        </div>
+      )
+    }
+  }
 }
